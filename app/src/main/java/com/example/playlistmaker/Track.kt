0405244af47722429976efb7802b2bae0b196c43
timachoo <timofey@tchoo.ru>
2023-04-02 23:23:40 +0300

    fix: исправлены замечания по оформлению

--- app/src/main/java/com/example/playlistmaker/Track.kt
+++ app/src/main/java/com/example/playlistmaker/Track.kt
@@ -1,8 +1,8 @@
 package com.example.playlistmaker
 
 data class Track(
-    val trackName: String // Название композиции
-    , val artistName: String // Имя исполнителя
-    , val trackTime: String // Продолжительность трека
-    , val artworkUrl100: String // Ссылка на изображение обложки
+    val trackName: String, // Название композиции
+    val artistName: String, // Имя исполнителя
+    val trackTime: String, // Продолжительность трека
+    val artworkUrl100: String // Ссылка на изображение обложки
 )
