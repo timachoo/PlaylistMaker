b77054ef04bf737b0f2a8e8595d4c379d03c2434
timachoo <timofey@tchoo.ru>
2023-05-18 17:01:58 +0300

    fix: Нажатие на элемент списка через лямбду

--- app/src/main/java/com/example/playlistmaker/TrackAdapter.kt
+++ app/src/main/java/com/example/playlistmaker/TrackAdapter.kt
@@ -1,13 +1,12 @@
 package com.example.playlistmaker
 
-import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
-import com.google.gson.Gson
 
-class TrackAdapter: RecyclerView.Adapter<TrackViewHolder> () {
+class TrackAdapter(
+    private var onItemClicked: ((movie: Track) -> Unit)
+) : RecyclerView.Adapter<TrackViewHolder> () {
 
     var trackList = ArrayList<Track>()
 
@@ -19,10 +18,7 @@ class TrackAdapter: RecyclerView.Adapter<TrackViewHolder> () {
     override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
         holder.bind(trackList[position])
         holder.itemView.setOnClickListener {
-            SearchHistory.add(trackList[position])
-            val displayIntent = Intent(it.context, PlayerActivity::class.java)
-            displayIntent.putExtra(PlayerActivity.TRACK_KEY, Gson().toJson(trackList[position]))
-            it.context.startActivity(displayIntent)
+            onItemClicked(trackList[position])
         }
     }
 
